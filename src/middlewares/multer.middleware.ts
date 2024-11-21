612bd92e95f6efd4e2011f656f8d0906c03cbb97
hildanku <hildankutomo@gmail.com>
2024-11-21 17:24:52 +0700

    fix(multer.middleware): fix error write_after_end code:ERR_STREAM_WRITE_AFTER_END
    - Changed the return type of the handleMulterError middleware from VOID to ANY to remove ts type checking.
    - Prevented write_after_end error caused by sending a response after the response stream has already been finalized.
    - Ensured the response is correctly handled and doesn't trigger the next middleware after sending the error response.

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,17 +1,17 @@
 import { type Request, type Response, type NextFunction } from 'express';
 import { MulterError } from 'multer';
 
-const handleMulterError = (err: MulterError, req: Request, res: Response, next: NextFunction): void => {
+const handleMulterError = (err: MulterError, req: Request, res: Response, next: NextFunction): any => {
     if (err instanceof MulterError) {
       
         if (err.code === 'LIMIT_FILE_SIZE') {
-            res.status(413).json({
+        return res.status(413).json({
                 status: 'fail',
                 message: 'Payload content length greater than maximum allowed: 1000000' 
             });
         }
 
-        res.status(400).json({ error: err.message });
+        return res.status(400).json({ error: err.message });
     }
     next(err);
 };

c4b66d96612bd539ddcc09319151d564144821f3
hildanku <hildankutomo>
2024-11-27 20:13:20 +0700

    feat(app-sidebar): added title nav

--- frontend_v2/src/components/app-sidebar.tsx
+++ frontend_v2/src/components/app-sidebar.tsx
@@ -4,39 +4,25 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Home, ScanSearch } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 
 // Menu items.
 const items = [
   {
-    title: "Predict",
-    url: "/predict",
+    title: "Home",
+    url: "/",
     icon: Home,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+    title: "Predict",
+    url: "/predict",
+    icon: ScanSearch,
   },
 ]
 
@@ -44,6 +30,24 @@ export function AppSidebar() {
   const location = useLocation();
   return (
     <Sidebar>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                { /* <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div> */ }
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Asclepius</span>
+                  <span className="">Detect the anomalies!</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        { /* <Search /> */}
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
